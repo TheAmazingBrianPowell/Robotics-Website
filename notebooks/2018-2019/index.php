d2650d33f50dc4c8dccb4f8f12f74c131e1d47c3
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2019-12-07 16:50:07 -0800

    Delete index.php

--- notebooks/2018-2019/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-  //Hey not much here...
-  //But wait..
-  //This gets a full page from years.php, its better than copy and paste, must I do the poetry again... no.
-  require $_SERVER['DOCUMENT_ROOT'] . '/notebooks/years.php';
-?>
