39209207b1b2f637adeecbf7065f9888d85befee
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2020-01-27 18:38:07 -0800

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <link rel="icon" href="resources/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="rhs, robotics, roseburg, high school, FTC, tech">
-    <meta name="google-site-verification" content="iY1Iy6FYXtVrRYqoOgU67hnFqe85uDv51zZ0Bu5YD7M">
+    <meta name="google-site-verification" content="O6-Dc-Hyl5fwdAXeFvv5bVHZvjdmJO4JhK6cdQdM_zg" />
     <meta name="description" content="The RHS Robotics program is a competitive, student run club that allows for unique experience of the FIRST Tech Challenge Program, and enables students to design, build, program, and compete in robotics">
   </head>
 <!--
