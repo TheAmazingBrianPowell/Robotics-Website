9199f9433ce86e86dd7ca63a8a3551c9c92e0c30
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2020-02-08 10:19:11 -0800

    Update index.php

--- photos/index.php
+++ photos/index.php
@@ -69,7 +69,7 @@
         $prevName = str_replace('/',' ',str_replace('_',' ',preg_replace('/^..([^\/]*)\/(.*)\/[^\/]*$/', '$1 $2', $filename))); 
         //get all images and display them as ... IMAGES!!!
         if(substr($filename, -3) == 'jpg' || substr($filename, -3) == 'JPG') {
-          echo '<div><a href = ' . substr($filename, 2) . ' target = "_blank"><img src = "/resources/loading.gif" data-src = ' . substr($filename, 2) . ' alt = "' . str_replace('_', ' ', substr($filename,strpos($filename, '/', 12) + 1, -4)) . '"></a></div>';
+          echo '<div><a href = ' . substr($filename, 2) . ' target = "_blank"><img src = "/resources/loading.svg" data-src = ' . substr($filename, 2) . ' alt = "' . str_replace('_', ' ', substr($filename,strpos($filename, '/', 12) + 1, -4)) . '"></a></div>';
           continue;
         }
         //get all (mp3?) videos and display in a video element
