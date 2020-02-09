b76c07e4bfca6841b4b137200a2c2089b989cad4
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2020-02-08 18:43:56 -0800

    Add files via upload

---
+++ photos/2018-2019/Competition/script3.js
@@ -0,0 +1,27 @@
+var a = document.getElementsByTagName('select')[0];
+a.value = window.location.pathname.substring(18, window.location.pathname.length-1).replace(new RegExp('_', 'g'), ' ');
+a.addEventListener('change', function() {
+  if(a.value != 'All Years') {
+    if(a.value[0] != 2) {
+      //.. goes to parent directory, that way it looks for an event in the years directory, not the current event directory that doesn't contain the other events
+      window.location = '../' + a.value.replace(new RegExp(' ', 'g'), '_');
+    }else{
+      window.location = '/photos/' + a.value.replace(new RegExp(' ', 'g'), '_');
+    }
+  }else{
+    window.location = "/photos/";
+  }
+});
+
+//Lazy loading technique, loads images as the user scrolls
+var imgs = document.getElementsByTagName('img');
+var divs = document.getElementsByTagName('div');
+window.addEventListener("scroll", LoadTheStuff);
+LoadTheStuff();
+function LoadTheStuff() {
+    for(var i = 2; i < imgs.length; i++) {
+      if(divs[i-2].getBoundingClientRect().top < (window.innerHeight + 300 || document.documentElement.clientHeight + 300) && divs[i-2].getBoundingClientRect().bottom > -300) {
+        imgs[i].setAttribute('src', imgs[i].getAttribute('data-src'));
+      }
+    }
+};
